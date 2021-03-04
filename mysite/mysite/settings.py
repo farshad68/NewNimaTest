486573ff347da46b2eea47640d6439a13255ba21
farshad68 <farshad.afshari68@gmail.com>
2021-03-04 22:40:33 +0100

    add exer

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #own
+    'exercise',
+    'video',
+    'submitedExercise'
 ]
 
 MIDDLEWARE = [
---
+++ mysite/submitedExercise/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SubmitedexerciseConfig(AppConfig):
+    name = 'submitedExercise'
